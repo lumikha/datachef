b6cd6da23000d84156402bd2ff4d90e15438f6d5
Bethany Joyce Sibala <bethany@lumikha.co>
2017-01-23 13:47:45 +0800

    responsive 20%
    ...

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 	<title> DataChef</title>
 	<link rel="Shortcut icon" href="assets/img/fav_32.png"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/"> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/"> 
 	<link rel="stylesheet" type="text/css" href="style.css"><!--main stylesheet-->
 
 	 <!--media queries-->
 	
-    <!--<link rel="stylesheet" type="text/css" href="assets/css/mobile_landscape.css"> <!--480-768-->
-    <!--<link rel="stylesheet" type="text/css" href="assets/css/small_tablet_portrait.css"> <!--600-800 additional breakpoint-->
+    <link rel="stylesheet" type="text/css" href="assets/css/mobile_landscape.css"> <!--480-768-->
+    <link rel="stylesheet" type="text/css" href="assets/css/small_tablet_portrait.css"> <!--600-800 additional breakpoint-->
   	<!--<link rel="stylesheet" type="text/css" href="assets/css/tablet_portrait.css"> <!--768-1024-->
    	<!--<link rel="stylesheet" type="text/css" href="assets/css/tablet_landscape.css"> <!--1024-1200-->
    	<!--<link rel="stylesheet" type="text/css" href="assets/css/desktop.css"> <!--min 1200-->
@@ -25,7 +25,7 @@
 <body>
 
 <div class="main">
-	<div class="h1"><strong>DATACHEF</strong></div>
+	<div class="h1"><strong>DATASCHEF</strong></div>
 	<div class="h2">
 		<p>Tasty data processing services for contact centers and digital marketers</p>
 		<p>First Seating: Q2 2017</p>
