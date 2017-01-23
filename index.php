9ee5b26482e3b543c437222ca74b79603f27489c
Bethany Joyce Sibala <bethany@lumikha.co>
2017-01-23 11:21:20 +0800

    datachef 50%
    :dancer:

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> DataChef</title>
+	<link rel="Shortcut icon" href="assets/img/fav_32.png"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/"> -->
+	<link rel="stylesheet" type="text/css" href="style.css"><!--main stylesheet-->
+
+	 <!--media queries-->
+	
+    <!--<link rel="stylesheet" type="text/css" href="assets/css/mobile_landscape.css"> <!--480-768-->
+    <!--<link rel="stylesheet" type="text/css" href="assets/css/small_tablet_portrait.css"> <!--600-800 additional breakpoint-->
+  	<!--<link rel="stylesheet" type="text/css" href="assets/css/tablet_portrait.css"> <!--768-1024-->
+   	<!--<link rel="stylesheet" type="text/css" href="assets/css/tablet_landscape.css"> <!--1024-1200-->
+   	<!--<link rel="stylesheet" type="text/css" href="assets/css/desktop.css"> <!--min 1200-->
+   	<!--<link rel="stylesheet" type="text/css" href="assets/css/large_desktop.css"> <!--min 1200-->
+
+   	<!--php scripts-->
+
+</head>
+<body>
+
+<div class="main">
+	<div class="h1">DATACHEF</div>
+	<div class="h2">
+		<p>Tasty Data Processing services for contact centers and digital marketers</p>
+		<p>First Seating: Q2 2017</p>
+	</div>
+
+	<div class="favicon">
+		<p><img src="assets/img/dchf_icon.png"></p>
+	</div>
+
+
+</div>
+
+<div class="footer">
+	<p>Copyright &copy 2017 <a href="http://lumikha.co">Atelier Lumikha.</a> All rights Reserved.</p>
+</div>
+
+
+</body>
+
+
+
+</html>
